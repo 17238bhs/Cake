0491faf50dddfdab2687f37e6931749bbba1ffc1
Leonard Lau <17238@burnside.school.nz>
2020-04-21 23:38:29 +1200

    initial commit

---
+++ routes.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+@app.route('/')
+def home():
+    return render_template('home.html', title="boop")
+
+@app.route('/donut')
+def donut():
+    return render_template('donut.html', title="bipbip")
+
+@app.route('/all_pizzas')
+def all_pizzas():
+    conn = sqlite3.connect('c:\\Users\\17238\\Desktop\\Flask\\Pizza\\Pizzadb.db') #TODO get /Pizza/Pizzadb.db to work
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Pizza;')
+    results = cur.fetchall()
+    #cur.execute('SELECT id FROM Pizza;')
+    #num = cur.fetchall()
+    return render_template('all_pizzas.html', pizzas=results) #, var=num
+
+@app.route('/pizza/<int:id>')
+def pizza_name(id):
+    print("DEBUG: I got pizza id {}".format(id)) #TODO DEBUG
+    conn = sqlite3.connect('c:\\Users\\17238\\Desktop\\Flask\\Pizza\\Pizzadb.db') #'H:/12DTP/Pizza/Pizzadb.db'
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Pizza WHERE id={}".format(id))
+    results = cur.fetchone()
+    cur.execute("SELECT name FROM Topping WHERE id IN (SELECT tid FROM PizzaTopping WHERE pid={})".format(id))
+    details = cur.fetchall()
+    return render_template('pizza.html', pizzas = results, toppings = details)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000, host='0.0.0.0')
